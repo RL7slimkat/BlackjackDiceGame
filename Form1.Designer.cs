b0d5c4dfd41301c9a75c8d6ad85eb1c4f3133d03
Rene P Lisasi <renelisasi@hotmail.com>
2021-02-17 03:29:20 -0500

    C# commit
    contains C# files from visual studio and test result document. Please visit Master branch for the entire project.

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿using System;
+
+namespace Blackjack21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.handTimer = new System.Windows.Forms.Timer(this.components);
+            this.diceThrowTimer = new System.Windows.Forms.Timer(this.components);
+            this.dicePickupTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.diceBox2 = new System.Windows.Forms.PictureBox();
+            this.diceBox1 = new System.Windows.Forms.PictureBox();
+            this.handGif = new System.Windows.Forms.PictureBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.diceBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diceBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.handGif)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(742, 111);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "BlackJack Dice";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPlay.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnPlay.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.btnPlay.FlatAppearance.BorderSize = 0;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlay.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(-4, 554);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(769, 69);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // handTimer
+            // 
+            this.handTimer.Enabled = true;
+            this.handTimer.Interval = 5200;
+            this.handTimer.Tick += new System.EventHandler(this.handTimer_Tick);
+            // 
+            // diceThrowTimer
+            // 
+            this.diceThrowTimer.Enabled = true;
+            this.diceThrowTimer.Interval = 2000;
+            this.diceThrowTimer.Tick += new System.EventHandler(this.diceThrowTimer_Tick);
+            // 
+            // dicePickupTimer
+            // 
+            this.dicePickupTimer.Enabled = true;
+            this.dicePickupTimer.Interval = 3425;
+            this.dicePickupTimer.Tick += new System.EventHandler(this.dicePickupTimer_Tick);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblResult.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.YellowGreen;
+            this.lblResult.Location = new System.Drawing.Point(12, 172);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(267, 223);
+            this.lblResult.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExit.BackColor = System.Drawing.Color.Black;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(0)))), ((int)(((byte)(45)))));
+            this.btnExit.Location = new System.Drawing.Point(-4, 554);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 69);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHelp.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnHelp.FlatAppearance.BorderSize = 0;
+            this.btnHelp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHelp.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.ForeColor = System.Drawing.Color.Black;
+            this.btnHelp.Location = new System.Drawing.Point(690, 554);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 69);
+            this.btnHelp.TabIndex = 7;
+            this.btnHelp.Text = "?";
+            this.btnHelp.UseVisualStyleBackColor = false;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // diceBox2
+            // 
+            this.diceBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.diceBox2.BackgroundImage = global::Blackjack21.Properties.Resources.transparent;
+            this.diceBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.diceBox2.Image = global::Blackjack21.Properties.Resources.rightDice6;
+            this.diceBox2.Location = new System.Drawing.Point(419, 445);
+            this.diceBox2.Name = "diceBox2";
+            this.diceBox2.Size = new System.Drawing.Size(82, 100);
+            this.diceBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diceBox2.TabIndex = 5;
+            this.diceBox2.TabStop = false;
+            // 
+            // diceBox1
+            // 
+            this.diceBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.diceBox1.BackgroundImage = global::Blackjack21.Properties.Resources.transparent;
+            this.diceBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.diceBox1.Image = global::Blackjack21.Properties.Resources.leftDice6;
+            this.diceBox1.InitialImage = null;
+            this.diceBox1.Location = new System.Drawing.Point(320, 427);
+            this.diceBox1.Name = "diceBox1";
+            this.diceBox1.Size = new System.Drawing.Size(93, 100);
+            this.diceBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.diceBox1.TabIndex = 4;
+            this.diceBox1.TabStop = false;
+            // 
+            // handGif
+            // 
+            this.handGif.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.handGif.Image = global::Blackjack21.Properties.Resources.RlY0;
+            this.handGif.InitialImage = global::Blackjack21.Properties.Resources.RlY0;
+            this.handGif.Location = new System.Drawing.Point(285, 65);
+            this.handGif.Name = "handGif";
+            this.handGif.Size = new System.Drawing.Size(480, 480);
+            this.handGif.TabIndex = 2;
+            this.handGif.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(766, 621);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.diceBox2);
+            this.Controls.Add(this.diceBox1);
+            this.Controls.Add(this.handGif);
+            this.Controls.Add(this.btnPlay);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Blackjack Dice";
+            ((System.ComponentModel.ISupportInitialize)(this.diceBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.diceBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.handGif)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void DicePickupTimer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DiceThrowTimer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandTimer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.PictureBox handGif;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.PictureBox diceBox1;
+        private System.Windows.Forms.PictureBox diceBox2;
+        private System.Windows.Forms.Timer handTimer;
+        private System.Windows.Forms.Timer diceThrowTimer;
+        private System.Windows.Forms.Timer dicePickupTimer;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHelp;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*author: RLisasi
+ * channel: RL7_slimkat video: Blackjack dice c# game demo
+https://youtu.be/j06kMyWUe_8
+ */
+namespace Blackjack21
+{
+    public partial class Form1 : Form
+    {
+        //Result of the dice roll property
+        private static int result1, result2, totalSum, sum = 0;
+        Random rnd = new Random();
+
+        //attach images to image objects
+        Bitmap leftDice1 = Properties.Resources.leftDice1;
+        Bitmap leftDice2 = Properties.Resources.leftDice2;
+        Bitmap leftDice3 = Properties.Resources.leftDice3;
+        Bitmap leftDice4 = Properties.Resources.leftDice4;
+        Bitmap leftDice5 = Properties.Resources.leftDice5;
+        Bitmap leftDice6 = Properties.Resources.leftDice6;
+
+        Bitmap rightDice1 = Properties.Resources.rightDice1;
+        Bitmap rightDice2 = Properties.Resources.rightDice2;
+        Bitmap rightDice3 = Properties.Resources.rightDice3;
+        Bitmap rightDice4 = Properties.Resources.rightDice4;
+        Bitmap rightDice5 = Properties.Resources.rightDice5;
+        Bitmap rightDice6 = Properties.Resources.rightDice6;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //set everything to 0
+            setNull();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //timers for animations
+            if (dicePickupTimer.Enabled == false && handTimer.Enabled == false && diceThrowTimer.Enabled == false)
+            {
+                dicePickupTimer.Enabled = true;
+                handTimer.Enabled = true;
+                diceThrowTimer.Enabled = true;
+            }
+            //only show 1 dice after 15
+            if (totalSum >= 15)
+            {
+                rollDice1();
+                totalSum += sum;
+                diceBox2.Image = null;
+            }
+            //continue to roll before 15
+            if (totalSum < 15)
+            {
+                rollDice2();
+                totalSum += sum;
+            }
+
+
+            handGif.Enabled = true;
+
+            //show message when you win
+            if (totalSum == 21)
+            {
+                totalSum = 0;
+                sum = 0;
+                lblResult.Text = "WIN";
+                // exit confirmation prompt
+                DialogResult confirmExit = MessageBox.Show("You hit 21, smash yes to replay or no to exit",
+                "WIN", MessageBoxButtons.YesNo);
+
+                if (confirmExit == DialogResult.Yes)
+                {
+                    //close the message box
+                    setNull();
+                    //enable button
+                    btnPlay.Enabled = true;
+                }
+                else
+                {
+                    //exit
+                    this.Close();
+                }
+            }
+            //hhandle losing
+            if (totalSum > 21)
+            {
+                totalSum = 0;
+                sum = 0;
+                lblResult.Text = "BUST";
+                // exit confirmation prompt
+                DialogResult confirmExit = MessageBox.Show("You went over 21, smash yes to replay or no to exit",
+                "BUST", MessageBoxButtons.YesNo);
+
+                if (confirmExit == DialogResult.Yes)
+                {
+                    //close the message box
+                    setNull();
+                    //enable button
+                    btnPlay.Enabled = true;
+                }
+                else
+                {
+                    //exit
+                    this.Close();
+                }
+
+            }
+
+
+        }
+        //timer for the hand
+        private void handTimer_Tick(object sender, EventArgs e)
+        {
+            handGif.Enabled = false;
+            handTimer.Enabled = false;
+            btnPlay.Enabled = true;
+        }
+        //timer for picking up the dice
+        private void dicePickupTimer_Tick(object sender, EventArgs e)
+        {
+            diceBox1.Image = null;
+            diceBox2.Image = null;
+            diceBox1.Visible = false;
+            diceBox2.Visible = false;
+            dicePickupTimer.Enabled = false;
+        }
+        //timer for throwing the dice
+        private void diceThrowTimer_Tick(object sender, EventArgs e)
+        {
+            setDiceImg();
+            diceThrowTimer.Enabled = false;
+            lblResult.Text = (totalSum).ToString();
+            diceBox1.Visible = true;
+            diceBox2.Visible = true;
+        }
+        //what happens when you roll 2 dice
+        public void rollDice2()
+        {
+            //Generate random number between 1 and 6 for roll 1
+            result1 = rnd.Next(1, 7);
+            result2 = rnd.Next(1, 7);
+            sum = result1 + result2;
+            Console.WriteLine($"you rolled 2 dice of {sum} and your total is {totalSum + sum}");
+            //disable button while dice is being thrown
+            btnPlay.Enabled = false;
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            // exit confirmation prompt
+            DialogResult confirmExit = MessageBox.Show("You roll dice in an attempt to hit exactly 21.  If you go over 21, you lose.  If you hit 21 exactly, you win. At first you will roll 2 dice.  Once your total hits 15 or above, you will only roll one dice.",
+            "HELP", MessageBoxButtons.OK);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // exit confirmation prompt
+            DialogResult confirmExit = MessageBox.Show("Are you sure you'd like to exit?",
+            "QUIT", MessageBoxButtons.YesNo);
+
+            if (confirmExit == DialogResult.Yes)
+            {
+                //exit
+                this.Close();
+            }
+            else
+            {
+                //return to game
+            btnPlay.Enabled = true;
+            }
+
+        }
+
+        //what happens when you roll 1 die
+        public void rollDice1()
+        {
+            result2 = 0;
+            result1 = rnd.Next(1, 7);
+            sum = result1;
+            Console.WriteLine($"you rolled 1 die of {sum} and your total is {totalSum + sum}");
+            //enable button
+            btnPlay.Enabled = false;
+        }
+        //reset everything
+        public void setNull()
+        {
+            handGif.Enabled = false;
+            dicePickupTimer.Enabled = false;
+            handTimer.Enabled = false;
+            diceThrowTimer.Enabled = false;
+            diceBox1.Image = null;
+            diceBox2.Image = null;
+            totalSum = sum = 0;
+            lblResult.Text = (totalSum + sum).ToString();
+            //enable button
+            btnPlay.Enabled = true;
+        }
+        //set the image according to the result
+        public void setDiceImg()
+        {
+            switch (result1)
+            {
+                //set dice 1
+                case 1:
+                    diceBox1.Image = leftDice1;
+                    break;
+                case 2:
+                    diceBox1.Image = leftDice2;
+                    break;
+                case 3:
+                    diceBox1.Image = leftDice3;
+                    break;
+                case 4:
+                    diceBox1.Image = leftDice4;
+                    break;
+                case 5:
+                    diceBox1.Image = leftDice5;
+                    break;
+                case 6:
+                    diceBox1.Image = leftDice6;
+                    break;
+                default:
+                    diceBox1.Image = null;
+                    break;
+            }
+            //set dice 2
+            switch (result2)
+            {
+                case 1:
+                    diceBox2.Image = rightDice1;
+                    break;
+                case 2:
+                    diceBox2.Image = rightDice2;
+                    break;
+                case 3:
+                    diceBox2.Image = rightDice3;
+                    break;
+                case 4:
+                    diceBox2.Image = rightDice4;
+                    break;
+                case 5:
+                    diceBox2.Image = rightDice5;
+                    break;
+                case 6:
+                    diceBox2.Image = rightDice6;
+                    break;
+                default:
+                    diceBox2.Image = null;
+                    break;
+            }
+        }
+    }
+}
